13095cba636c37cdc47c5e161199580a8b789860
uchidash <uchidash456@gmail.com>
2023-05-17 16:37:21 +0900

    update app

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use aws_sdk_s3::{Client, Error};
 use aws_types::region::Region;
+use clap::Parser;
 use dialoguer::{theme::ColorfulTheme, FuzzySelect};
 
-async fn prepare_client() -> Client {
-    let region_name = "ap-northeast-1";
+#[derive(Parser)]
+struct Args {
+    /// AWS Region
+    #[arg(short, long, default_value = "ap-northeast-1")]
+    region: String,
+
+    /// デバッグモード
+    #[arg(short, long)]
+    debug: bool,
+}
+
+/// AWSクライアントを準備
+async fn prepare_client(region: &str) -> Client {
     let config = aws_config::from_env()
-        .region(Region::new(region_name))
+        .region(Region::new(region.to_string()))
         .load()
         .await;
     return Client::new(&config);
@@ -13,14 +25,33 @@ async fn prepare_client() -> Client {
 
 /// バケット名一覧を取得
 async fn list_buckets(client: &Client) -> Vec<String> {
-    let resp = client.list_buckets().send().await.unwrap();
-    let objects = resp.buckets().unwrap();
-    let mut buckets: Vec<String> = Vec::with_capacity(objects.len());
-    for object in objects {
-        let bucket = object.name().unwrap();
-        buckets.push(bucket.to_string());
+    let resp = client.list_buckets().send().await;
+    match resp {
+        Ok(output) => {
+            let objects = output.buckets().unwrap();
+            let mut buckets: Vec<String> = Vec::with_capacity(objects.len());
+            for object in objects {
+                let bucket = object.name().unwrap();
+                buckets.push(bucket.to_string());
+            }
+            return buckets;
+        }
+        Err(error) => {
+            eprintln!("エラー: {}", error);
+            return Vec::new();
+        }
     }
-    return buckets;
+}
+
+/// バケット一覧の取得と選択
+async fn select_bucket(client: &Client) -> String {
+    let buckets = list_buckets(&client).await;
+    let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
+        .items(&buckets)
+        .default(0)
+        .interact();
+    let bucket = buckets[selection.unwrap()].to_string();
+    return bucket;
 }
 
 /// バケット内のオブジェクト一覧を取得
@@ -32,66 +63,87 @@ async fn list_objects(client: &Client, bucket: &str, prefix: &str) -> Vec<String
         .prefix(prefix)
         .delimiter("/") // 1階層のみ対象にする
         .send()
-        .await
-        .unwrap();
+        .await;
 
-    let common_prefixes = resp.common_prefixes();
-    if common_prefixes.is_none() {
-        // ファイル一覧を取得
-        let mut prefixes = Vec::with_capacity(resp.contents().unwrap().len());
-        for object in resp.contents().unwrap() {
-            let prefix = object.key().unwrap();
-            prefixes.push(prefix.to_string());
+    match resp {
+        Ok(output) => {
+            let common_prefixes = output.common_prefixes();
+            if common_prefixes.is_none() {
+                // ファイル一覧を取得
+                let contents = output.contents().unwrap();
+                let mut prefixes = Vec::with_capacity(contents.len());
+                for object in contents {
+                    let prefix = object.key().unwrap();
+                    prefixes.push(prefix.to_string());
+                }
+                return prefixes;
+            } else {
+                // ディレクトリ一覧を取得
+                let mut prefixes = Vec::with_capacity(common_prefixes.unwrap().len());
+                for object in common_prefixes.unwrap() {
+                    let prefix = object.prefix().unwrap();
+                    prefixes.push(prefix.to_string());
+                }
+                return prefixes;
+            }
         }
-        return prefixes;
-    } else {
-        // ディレクトリ一覧を取得
-        let mut prefixes = Vec::with_capacity(common_prefixes.unwrap().len());
-        for object in common_prefixes.unwrap() {
-            let prefix = object.prefix().unwrap();
-            prefixes.push(prefix.to_string());
+        Err(error) => {
+            eprintln!("エラー: {}", error);
+            return Vec::new();
         }
-        return prefixes;
     }
+}
+
+/// オブジェクトが一覧の取得と選択
+async fn select_object(client: &Client, bucket: &str, prefix: &str) -> String {
+    let objects = list_objects(&client, bucket, prefix).await;
+
+    let prefixes: Vec<&str> = objects.iter().map(|p| p.as_str()).collect();
+    let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
+        .items(&prefixes)
+        .default(0)
+        .interact();
+
+    let selected_prefix = prefixes[selection.unwrap()];
+    return selected_prefix.to_string();
+}
+
+/// AWS Management Console で表示するための URI を作成
+async fn make_uri(bucket: &str, prefix: &str, region: &str) -> String {
+    if prefix.ends_with("/") {
+        return String::new();
+    }
+
+    let uri = format!(
+        "https://s3.console.aws.amazon.com/s3/object/{}?prefix={}&region={}",
+        bucket, prefix, region
+    );
+    return uri;
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let client = prepare_client().await;
+    let args = Args::parse();
+    let region = &args.region;
 
-    // バケット名一覧を取得
-    let buckets = list_buckets(&client).await;
-    let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
-        .items(&buckets)
-        .default(0)
-        .interact();
-    let bucket = buckets[selection.unwrap()].as_str();
+    // バケット一覧から対象のバケットを選択
+    let client = prepare_client(region).await;
+    let bucket = select_bucket(&client).await;
 
     let mut prefix = String::new();
+    let mut uri: String;
     loop {
+        // 1階層下のオブジェクト一覧を表示・選択
         let current_input = prefix.as_str();
-        let objects = list_objects(&client, bucket, current_input).await;
-        let prefixes: Vec<&str> = objects.iter().map(|s| s.as_str()).collect();
-
-        // 候補リストがある場合は、選択可能なUI要素に表示する
-        if !prefixes.is_empty() {
-            // ユーザーに選択させるためのインタラクティブな選択肢を作成
-            let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
-                .items(&prefixes)
-                .default(0)
-                .interact();
-
-            // ユーザーが候補リストから選択した場合は、その選択内容を現在の入力欄に自動的に挿入する
-            let selected_prefix = prefixes[selection.unwrap()];
-            if prefixes.contains(&selected_prefix) {
-                prefix = format!(
-                    "{}{}",
-                    current_input,
-                    &selected_prefix[current_input.len()..]
-                );
-            }
+        prefix = select_object(&client, bucket.as_str(), current_input).await;
+        uri = make_uri(bucket.as_str(), &prefix, region).await;
+        if !uri.is_empty() {
+            break;
         }
     }
+
+    // URIを表示
+    println!("URI: {}", uri);
 
     Ok(())
 }
